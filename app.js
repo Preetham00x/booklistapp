8a3e72e6f61987f93a42dc5f7ed2a0caf279a66d
Preetham00x <preetham2006@outlook.com>
2026-01-03 22:02:33 +0530

    3

--- app.js
+++ app.js
@@ -5,3 +5,18 @@ class Book {
         this.author=author;
         this.isbn=isbn;
     }}
+//ui class to handle ui tasks
+class UI{
+    static displayBooks(){
+        const Storedbook=[{
+            title:'Book One',  
+            author:'John Doe',
+            isbn:'343434',
+        },
+        {
+            title:'Book Two',   
+            author:'Jane Doe',
+            isbn:'454545',}
+        ];
+}
+}
