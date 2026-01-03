0fb180783527555d6537052213810a9fc0ef908f
Preetham00x <preetham2006@outlook.com>
2026-01-03 22:44:08 +0530

    3

--- app.js
+++ app.js
@@ -23,5 +23,13 @@ class UI{
 }
 static addBookList(book){
     const list=doscument.getElementById('book-list');
+    const row = document.createElement('tr');
+    row.innerHTML=`
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+    `;
+    list.appendChild(row);
     
-}
+}}
