27ff344bdc0f1ddca0cde14c8e767bc0a9c0ba31
Preetham00x <preetham2006@outlook.com>
2026-01-05 00:44:11 +0530

    1

--- legacy/app.js
+++ legacy/app.js
@@ -54,6 +54,32 @@ static clearFields(){
         document.querySelector('#isbn').value='';
     };
 };
+class Store{
+    static getbooks(){
+        let books;
+        if(localStorage.getItem('books')===null){
+            books=[];
+        }
+        else{
+            books=JSON.parse(localStorage.getItem('books'));
+        }
+        return books;
+    }
+    static addbook(book){
+        const books=Store.getbooks();
+        books.push(book);
+        localStorage.setItem('books',JSON.stringify(books));
+    }
+    static removebook(isbn){
+        const books=Store.getbooks();
+        books.forEach((book,index)=>{
+            if(book.isbn===isbn){
+                books.splice(index,1);
+            }
+        });
+        localStorage.setItem('books',JSON.stringify(books));
+    }
+}
 
 //event display book
 document.addEventListener('DOMContentLoaded',UI.displayBooks);
