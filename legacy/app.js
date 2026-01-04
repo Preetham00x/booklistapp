09f7022d33a0e2d3588307faa22b0dad12630b9c
Preetham00x <preetham2006@outlook.com>
2026-01-05 00:31:18 +0530

    1

--- legacy/app.js
+++ legacy/app.js
@@ -36,7 +36,19 @@ static deletbook(el){
     if(el.contains('delete')){
         el.parentElement.parentElement.remove();};
     }
-    static clearFields(){
+static showAlert(message,className){
+    const div=document.createElement('div');
+    div.className=`alert alert-${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container=document.querySelector('.container');
+    const form=document.querySelector('#book-form');
+    container.insertBefore(div,form);
+    //vanish in 3 seconds
+    setTimeout(()=>document.querySelector('.alert').remove(),3000);
+}
+
+
+static clearFields(){
         document.querySelector('#title').value='';
         document.querySelector('#author').value='';
         document.querySelector('#isbn').value='';
@@ -53,11 +65,14 @@ document.querySelector('#book-form').addEventListener('submit', (e)=>{
     const title=document.querySelector('#title').value;
     const author=document.querySelector('#author').value;
     const isbn=document.querySelector('#isbn').value;
+    if(title=== '' || author==='' || isbn===''){
+        UI.showAlert("FIll in all FIELDS","danger");
+    }else{
     //instantiate book
     const book =new Book(title,author,isbn);
-    UI.addBookList(book);
-    console.log(book);
-});''
+    UI.addBookList(book);   
+    UI.clearFields();}
+});;
 //delete book event
 document.querySelector('#book-list').addEventListener('click',(e) =>{
     UI.deletbook(e.target)});
